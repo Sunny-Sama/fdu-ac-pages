1b3be91f345e24fc2911c8cbb5bee3b82e31a638
Sunny-Sama <13302010096@fudan.edu.cn>
2017-04-25 16:47:11 +0800

    前端完成，进行服务器端的部署

--- js/controllers/manage-controller.js
+++ js/controllers/manage-controller.js
@@ -4,6 +4,14 @@
 angular.module('ac-manage.controllers', [])
     .controller('acUserCtrl', function($rootScope, $scope, $http) {
 
+        var getTableIdList = function(){
+            var tableIds = '';
+            for(var i = 0; i < $rootScope.selectedTables.length; i++){
+                tableIds += $rootScope.selectedTables[i].tableId + ',';
+            }
+            return tableIds;
+        }
+
         $scope.whiteList = [{
             ruleId: '0',
             userName: 'user1' //可以是用户名也可以是用户id
@@ -29,15 +37,20 @@ angular.module('ac-manage.controllers', [])
             userName: 'user7'
         } ];
 
+        $scope.wholeWhite = $scope.whiteList;
+        $scope.wholeBlack = $scope.blackList;
+
+        // 获取当前表格的白名单列表
         var getWhiteList = function() {
-            //添加tableList参数
-            $http.get('http://' + $rootScope.hostUrl + '8080/program_name/package_name/getWhiteList')
+            var tables = getTableIdList();
+            $http.get('http://' + $rootScope.hostUrl + '8080/fdu_ac_service/getWhiteList', {param: {tableIds: tables}})
                 .success(function(ret) {
                     if (ret != null && ret[0] != null) {
                         $scope.whiteList = ret;
                     } else {
                         $scope.whiteList = null;
                     }
+                    $scope.wholeWhite = $scope.whiteList;
                 })
                 .error(function() {
                     alert('http error: 不能获取白名单');
@@ -45,18 +58,21 @@ angular.module('ac-manage.controllers', [])
         }
 
         var getBlackList = function() {
-            $http.get('http://' + $rootScope.hostUrl + '8080/DataACServer/getBlackList')
+            var tables = getTableIdList();
+            $http.get('http://' + $rootScope.hostUrl + '8080/fdu_ac_service/getBlackList', {param: {tableIds: tables}})
                 .success(function(ret) {
                     if (ret != null && ret[0] != null) {
                         $scope.blackList = ret;
                     } else {
                         $scope.blackList = null;
                     }
+                    $scope.wholeBlack = $scope.blackList;
                 })
                 .error(function() {
                     alert('http error: 不能获取黑名单');
                 });
         }
+
 
         $scope.searchWhite = function() {
             var searchKey = document.getElementById('ac-whiteKeyValue').value;
@@ -65,14 +81,20 @@ angular.module('ac-manage.controllers', [])
                     searchKey = searchKey.replace(' ', '');
                 }
                 if(searchKey.length == 0){
-                   alert('请输入查询内容');
+                    $scope.whiteList = $scope.wholeWhite;
                 }
                 else{
-                    //TODO：获取查询内容
+                    var tmp = new Array();
+                    for(var i = 0; i < $scope.wholeWhite.length; i++){
+                        if($scope.wholeWhite[i].userName.indexOf(searchKey) >= 0){
+                            tmp.push($scope.wholeWhite[i]);
+                        }
+                    }
+                    $scope.whiteList = tmp;
                 }
             }
             else {
-                alert('请输入查询内容');
+                $scope.whiteList = $scope.wholeWhite;
             }
         }
 
@@ -83,19 +105,26 @@ angular.module('ac-manage.controllers', [])
                     searchKey = searchKey.replace(' ', '');
                 }
                 if(searchKey.length == 0){
-                    alert('请输入查询内容');
+                    $scope.blackList = $scope.wholeBlack;
                 }
                 else{
-                    //TODO：获取查询内容
+                    var tmp = new Array();
+                    for(var i = 0; i < $scope.wholeBlack.length; i++){
+                        if($scope.wholeBlack[i].userName.indexOf(searchKey) >= 0){
+                            tmp.push($scope.wholeBlack[i]);
+                        }
+                    }
+                    $scope.blackList = tmp;
                 }
             }
             else {
-                alert('请输入查询内容');
+                $scope.blackList = $scope.wholeBlack;
             }
         }
 
         $scope.addWhite = function() {
             var userKey = document.getElementById('ac-addWhiteKey');
+            var tables = getTableIdList();
             if(userKey != null && userKey.length >0){
                 while(userKey.lastIndexOf(' ') >= 0){
                     userKey = userKey.replace(' ', '');
@@ -105,7 +134,15 @@ angular.module('ac-manage.controllers', [])
                 }
                 else{
                     document.getElementById('ac-white-error').innerHTML = '';
-                    //TODO：添加用户到白名单
+                    $http.get('http://' + $rootScope.hostUrl + '8080/fdu_ac_service/addWhiteList', {param: {tableIds: tables, userKey: userKey}})
+                        .success(function(ret) {
+                            // if user is invalid
+                            // if whiteList already exists
+                            // if success
+                        })
+                        .error(function() {
+                            alert('http error: 不能添加新的白名单');
+                        });
                 }
             }
             else {
@@ -114,7 +151,8 @@ angular.module('ac-manage.controllers', [])
         }
 
         $scope.addBlack = function() {
-            var userKey = document.getElementById('ac-addWhiteKey');
+            var userKey = document.getElementById('ac-addBlackKey');
+            var tables = getTableIdList();
             if(userKey != null && userKey.length >0){
                 while(userKey.lastIndexOf(' ') >= 0){
                     userKey = userKey.replace(' ', '');
@@ -124,20 +162,40 @@ angular.module('ac-manage.controllers', [])
                 }
                 else{
                     document.getElementById('ac-black-error').innerHTML = '';
-                    //TODO：添加用户到黑名单
+                    $http.get('http://' + $rootScope.hostUrl + '8080/fdu_ac_service/addBlackList', {param: {tableIds: tables, userKey: userKey}})
+                        .success(function(ret) {
+                            // if user is invalid
+                            // if whiteList already exists
+                            // if success
+                        })
+                        .error(function() {
+                            alert('http error: 不能添加新的黑名单');
+                        });
                 }
             }
             else {
-                 document.getElementById('ac-black-error').innerHTML = '请输入有效的用户名';
+                document.getElementById('ac-black-error').innerHTML = '请输入有效的用户名';
             }
         }
 
         $scope.deleteWhite = function(ruleId) {
+            $http.get('http://' + $rootScope.hostUrl + '8080/fdu_ac_service/deleteWhiteList', {param: {ruleId: ruleId}})
+                .success(function(ret) {
 
+                })
+                .error(function() {
+                    alert('http error: 不能删除白名单');
+                });
         }
 
         $scope.deleteBlack = function(ruleId){
+            $http.get('http://' + $rootScope.hostUrl + '8080/fdu_ac_service/deleteBlackList', {param: {ruleId: ruleId}})
+                .success(function(ret) {
 
+                })
+                .error(function() {
+                    alert('http error: 不能删除黑名单');
+                });
         }
     })
     .controller('acAttributeCtrl', function($rootScope, $scope, $http) {
